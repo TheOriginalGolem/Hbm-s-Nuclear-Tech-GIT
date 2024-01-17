ab85ca3367922c55caa2e1656cd84e4b74983ebf
GrafEnters <graf.enters@gmail.com>
2024-01-17 10:48:28 +0300

    Fixed rtgFurnace with hoppers & remade via TEMachineBase & optimized isUseableByPlayer

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineArcFurnace.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineArcFurnace.java
@@ -44,13 +44,9 @@ public class TileEntityMachineArcFurnace extends TileEntityMachineBase implement
 		return "container.arcFurnace";
 	}
 	
+	@Override
 	public boolean isUseableByPlayer(EntityPlayer player) {
-		if(world.getTileEntity(pos) != this)
-		{
-			return false;
-		}else{
-			return player.getDistanceSq(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D) <=64;
-		}
+		return checkUsableByPlayer(player, 64);
 	}
 	
 	@Override
